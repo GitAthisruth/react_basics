892439a6ac8c66fe3e65ef910ea70e2c3f6fadc2
Athisruth <athisruthmsatheesh98@gmail.com>
2025-01-18 22:37:25 +0530

    "Added"

--- src/message.tsx
+++ src/message.tsx
@@ -1,12 +1,21 @@
 //PascalCasing
 
+// function Message() {
+    // JSX: JavaScript XML
+    // return <h1>Hi AI Engineer</h1>
+// }
+
+//very basic component
+
+// export default Message;
+
 function Message() {
     // JSX: JavaScript XML
-    return <h1>Hi AI Engineer</h1>
+    const name = ' AI Engineer';
+    if (name)
+        return <h1>Hello Athisruth {name}</h1>
 }
 
 //very basic component
 
 export default Message;
-
-//snjasjkas
