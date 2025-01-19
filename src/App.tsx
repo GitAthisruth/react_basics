93c854d9575a7fac6f8c6a015c2798f4c7a7512a
Athisruth <athisruthmsatheesh98@gmail.com>
2025-01-19 11:19:47 +0530

    "added html contents using jsx"

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import Message from "./message";
+import ListGroup from "./components/ListGroup";
+
 
 
 function App() {
-  return <div><Message/></div>
+  return <div><ListGroup/></div>;
 }
 
 export default App;
